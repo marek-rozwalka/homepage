09e0420de5ae291c668e02d8dc750536201c2c63
Marek Rozwalka <rozwalka.marek@gmail.com>
2021-02-22 19:00:47 +0100

    change style and add javascripts

---
+++ main.js
@@ -0,0 +1,10 @@
+const firstName = "Marek";
+const age = 33;
+
+console.log(firstName);
+console.log(age);
+
+alert('siema UWAGA');
+console.log('heha');
+
+console.log(`Siema nazywam się ${firstName} i mam ${age} lata.`);
