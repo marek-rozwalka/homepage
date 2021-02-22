5e5b1545d2d1145490163ffa976905611e704d6b
Marek Rozwalka <rozwalka.marek@gmail.com>
2021-02-22 23:06:05 +0100

    change style to BEM terminology

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const firstName = "Marek";
-const age = 33;
+const firstName = "Kampik";
+const age = 2;
 
 console.log(firstName);
 console.log(age);
@@ -8,3 +8,9 @@ alert('siema UWAGA');
 console.log('heha');
 
 console.log(`Siema nazywam się ${firstName} i mam ${age} lata.`);
+
+const heading = document.querySelector('.main__heading--js');
+heading.innerHTML = `Siema nazywam się ${firstName} i mam ${age} lata.`;
+
+const emptyParagraph = document.querySelector('.week-summary__description--js');
+emptyParagraph.innerHTML = `Nawet uzupełniłem treść javascriptem!`;
