fffa89fcc98068016e262ad68cf8ded5a19f95a9
Marek Rozwalka <rozwalka.marek@gmail.com>
2021-02-26 21:50:43 +0100

    my first function

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ const age = 2;
 console.log(firstName);
 console.log(age);
 
-alert('siema UWAGA');
-console.log('heha');
+alert("siema UWAGA");
+console.log("heha");
 
 console.log(`Siema nazywam się ${firstName} i mam ${age} lata.`);
 
-const heading = document.querySelector('.main__heading--js');
+const heading = document.querySelector(".main__heading--js");
 heading.innerHTML = `Siema nazywam się ${firstName} i mam ${age} lata.`;
 
-const emptyParagraph = document.querySelector('.week-summary__description--js');
+const emptyParagraph = document.querySelector(".week-summary__description--js");
 emptyParagraph.innerHTML = `Nawet uzupełniłem treść javascriptem!`;
